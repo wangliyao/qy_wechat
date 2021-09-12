8df0e5314eb30362650afca56fe3ea739db7b667
wangliyao <755210570@qq.com>
2021-09-12 23:26:45 +0800

    Update qy_wechat_controller.rb

--- app/controllers/qy_wechat/qy_wechat_controller.rb
+++ app/controllers/qy_wechat/qy_wechat_controller.rb
@@ -5,8 +5,8 @@ module QyWechat
     include ReplyMessageHelper
 
     skip_before_action :verify_authenticity_token, only: :reply
-    before_filter :setup_qy_app, only: [:verify_url, :reply]
-    before_filter :setup_wechat_message, only: :reply
+    before_action :setup_qy_app, only: [:verify_url, :reply]
+    before_action :setup_wechat_message, only: :reply
 
     # 验证URL有效性
     # TODO: refactor
